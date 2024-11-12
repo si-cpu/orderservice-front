eb4fb25b2185c41a9e0d4fa7ce5fd451539e28d9
Si <tlgyrjs@naver.com>
2024-11-12 11:22:50 +0900

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './components/Home';
-import { Route, Routes } from 'react-router-dom';
-import MemberCreate from './components/MemberCreate';
-import LoginPage from './components/LoginPage';
 import { AuthContextProvider } from './context/UserContext';
+import { CartContextProvider } from './context/CartContext';
+import AppRouter from './router/AppRouter';
 
 function App() {
   return (
     <AuthContextProvider>
-      <div className='App'>
-        <Header />
-        <div className='content-wrapper'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/member/create' element={<MemberCreate />} />
-            <Route path='/login' element={<LoginPage />} />
-          </Routes>
+      <CartContextProvider>
+        <div className='App'>
+          <Header />
+          <div className='content-wrapper'>
+            <AppRouter />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </CartContextProvider>
     </AuthContextProvider>
   );
 }
